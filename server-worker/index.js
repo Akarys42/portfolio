f77371e9a6e61f74bf87cfb50e1a7e5fd6f28692
Matteo Bertucci <matteobertucci2004@gmail.com>
2021-06-01 12:57:24 +0000

    Add server worker

---
+++ server-worker/index.js
@@ -0,0 +1,64 @@
+var mimetypes = require("mime-types")
+
+const setKV = (key, data) => STORAGE.put(key, data)
+const getKV = (key) => STORAGE.get(key)
+const listKV = () => STORAGE.list()
+const deleteKV = (key) => STORAGE.delete(key)
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function uploadAsset(request) {
+  console.log("Updating assets")
+  if (request.headers.get("Authorization") !== DEPLOY_TOKEN) {
+    return new Response("Forbidden", {status: 403})
+  }
+
+  let data = {}
+  let formData = await request.formData()
+  for (const entry of formData.entries()) {
+    data[entry[0]] = entry[1]
+  }
+
+  let keys = Object.keys(data)
+
+  let existingKeys = (await listKV())["keys"].map(elem => elem["name"])
+
+  let keysToDelete = existingKeys.filter(elem => !(keys.includes(elem)))
+  for (const elem of keysToDelete) {
+    await deleteKV(elem)
+  }
+
+  for (const key of keys) {
+    await setKV(key, data[key])
+  }
+
+  return new Response(null, {status: 204})
+}
+
+async function handleRequest(request) {
+  let path = new URL(request.url).pathname
+  if (request.method === "POST" && path === "/upload") {
+    return uploadAsset(request)
+  }
+
+  if (request.method !== "GET") {
+    return new Response("Method not allowed", {status: 405})
+  }
+
+  // Special case for the root url
+  if (path === "/") {
+    path = "/index.html"
+  }
+
+  console.log(`Getting static at ${path}`)
+  let data = await getKV(path)
+  if (!data) {
+    return new Response('Not found', {status: 404})
+  }
+
+  return new Response(data, {
+    headers: { 'content-type': mimetypes.lookup(path) | "text/plain" },
+  })
+}
---
+++ upload.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+files=$(python - << "EOF"
+from pathlib import Path
+
+files = []
+
+def scan(folder: Path, path: str = ""):
+    for file in folder.iterdir():
+        if file.is_dir():
+            scan(file, f"{path}/{file.name}")
+        else:
+            files.append(f"{path}/{file.name}")
+
+scan(Path("./dist"))
+print("-F " + " -F ".join(f"{name}=@dist{name}" for name in files))
+EOF
+)
+
+code=$(curl -X POST --silent --output /dev/stderr --write-out "%{http_code}" -H "Authorization: ${DEPLOY_TOKEN}" $files https://akarys.me/upload)
+if [ $code -ne 204 ]; then
+  echo
+  echo "Server returned code $code"
+  exit 1
+fi
